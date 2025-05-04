c309b818861d4ea39dbf38ef2c2f3d1cb62a9951
Marco Túlio <marcopbreyner@gmail.com>
2025-05-04 01:15:00 -0300

    Aula 6
    Criando rotas para a primeira pagina HTML

--- Aulas/learning_log/learning_logs/views.py
+++ Aulas/learning_log/learning_logs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """"Pagina principal do Learning_Log"""
+    return render(request, 'learning_logs/index.html')
